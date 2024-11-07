46b3283e8a33a6043d006f9763a4c62bc7877a1b
prudhvi0906 <prudhvi9395@gmail.com>
2024-11-07 09:00:48 +0530

    security group

--- ec2.tf
+++ ec2.tf
@@ -2,4 +2,27 @@ resource "aws_instance" "example" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
 
-}  
+}
+
+resource "aws_security_group" "Roboshop_all" {
+  name        = "Roboshop_all_aws"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "Roboshop_all" {
+  security_group_id = aws_security_group.Roboshop_all.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "Roboshop_all" {
+  security_group_id = aws_security_group.Roboshop_all.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
